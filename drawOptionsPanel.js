627628670655c6cbc3270548bda32f88ca9ce890
Ben <asd@hhhh>
2015-01-19 17:08:32 +0000

    www

--- drawOptionsPanel.js
+++ drawOptionsPanel.js
@@ -1,3 +1,57 @@
-/**
- * Created by Ben on 29/12/2014.
- */
+function DrawOptionsPanel()
+{
+	this.containerDiv=document.createElement("div");
+	this.containerDiv.innerHTML=
+		'<div>'+
+
+		'	<div class="containerStrokeFill">'+
+			'		<span class="stroke-fill-span">Stroke</span>' +
+				'<input type="checkbox" id="checkStroke">'+
+			'		<span class="stroke-fill-span">Fill</span>' +
+				'<input type="checkbox" id="checkFill">'+
+		'	</div>'+
+		'	<div class="containerLineWidth">'+
+			'	Line Width'+
+			'	<div style="display: inline-block;">'+
+			'		<select id="dropDownBox" class="dropDownBox">'+
+			'			<option value="1">1</option>'+
+			'			<option value="2">2</option>'+
+			'			<option value="3">3</option>'+
+			'			<option value="4">4</option>'+
+			'			<option value="5">5</option>'+
+			'			<option value="6">6</option>'+
+			'			<option value="7">7</option>'+
+			'			<option value="8">8</option>'+
+			'			<option value="9">9</option>'+
+			'			<option value="10">10</option>'+
+			'		</select>'+
+			'	</div>'+
+		'	</div>'+
+		'</div>';
+	this.checkboxFill=this.containerDiv.querySelector("#checkFill");
+	this.checkbox=this.containerDiv.querySelector("#checkStroke");
+	this.checkbox.checked=true;
+
+	this.dropDownBox=this.containerDiv.querySelector(".dropDownBox");
+}
+
+DrawOptionsPanel.prototype.getElement=function()
+{
+	return this.containerDiv;
+}
+
+DrawOptionsPanel.prototype.isFill=function()
+{
+	return this.checkboxFill.checked;
+}
+
+DrawOptionsPanel.prototype.isStroke=function()
+{
+	return this.checkbox.checked;
+}
+
+DrawOptionsPanel.prototype.getLineWidth=function()
+{
+	return this.dropDownBox.options[this.dropDownBox.selectedIndex].value
+
+}
